daa0b15fee03fb48f8a0acfd8366a1441f44ee6e
ohmy207 <ohmy207@gmail.com>
2017-06-08 22:33:27 +0800

    made it work!

---
+++ run_master_servers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+steamcmd_dir="$HOME/steamcmd"
+install_dir="$HOME/dontstarvetogether_dedicated_server"
+cluster_name="MyDediServer"
+dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
+
+fail()
+{
+        echo Error: "$@" >&2
+        exit 1
+}
+
+check_for_file()
+{
+    if [ ! -e "$1" ]; then
+            fail "Missing file: $1"
+    fi
+}
+
+cd "$steamcmd_dir" || fail "Missing $steamcmd_dir directory!"
+
+check_for_file "steamcmd.sh"
+check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
+check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
+check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
+
+#run master and auto download mods frome Steam
+check_for_file "$install_dir/bin"
+cd "$install_dir/bin" || fail 
+./dontstarve_dedicated_server_nullrenderer -console -cluster $cluster_name -shard Master
--- update_dedicated_server.sh
+++ update_dedicated_server.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+#check
 steamcmd_dir="$HOME/steamcmd"
 install_dir="$HOME/dontstarvetogether_dedicated_server"
 cluster_name="MyDediServer"
 dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
 
-function fail()
+fail()
 {
-        echo Error: "$@" >&2
-        exit 1
+    echo Error: "$@" >&2
+    exit 1
 }
 
-function check_for_file()
+check_for_file()
 {
     if [ ! -e "$1" ]; then
-            fail "Missing file: $1"
+        fail "Missing file: $1"
     fi
 }
 
@@ -25,4 +26,7 @@ check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
 check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
 check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 
+#download and update
 ./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+
+check_for_file "$install_dir/bin"
